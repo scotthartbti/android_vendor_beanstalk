d67632db0b4c3f3e4c5673c578a67bf132e746af
Chris Sarbora <sarbs@cyngn.com>
2014-12-17 14:41:28 -0800

    Allow vendor/cyngn overlays to override vendor/cm overlays
    Change-Id: I4806f6ec8cd5b8171cbc4dac3cc1b93dbe941e92
    (cherry picked from commit 5c5a2e8f08f92b2d75a50c4cfc2707ac076f357f)

--- config/common.mk
+++ config/common.mk
@@ -235,4 +235,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 -include vendor/cyngn/product.mk
 
-$(call inherit-product-if-exists, vendor/extra/product.mk)
+$(call prepend-product-if-exists, vendor/extra/product.mk)
