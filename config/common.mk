5104066e4f347123554cad002bcb05a81f085ef9
scott <scott@ubuntu>
2015-01-17 23:06:46 -0500

    bump to beanstalk 5.0020
    Change-Id: I313348b48b2872f42b403370bcf9cd71b721e5d0
    Signed-off-by: scott <scott@ubuntu>

--- config/common.mk
+++ config/common.mk
@@ -214,7 +214,7 @@ PRODUCT_COPY_FILES += \
 	vendor/beanstalk/prebuilt/etc/hosts.alt:system/etc/hosts.alt \
 	vendor/beanstalk/prebuilt/etc/hosts.og:system/etc/hosts.og
 
-BeanStalk_Version=5.0015
+BeanStalk_Version=5.0020
 BS_VERSION := BeanStalk-$(BeanStalk_Version)-$(shell date -u +%Y%m%d)-$(BS_BUILD)
 
 PRODUCT_PROPERTY_OVERRIDES += \
