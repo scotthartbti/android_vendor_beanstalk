a79cdda4b0b7c52c2a5badb76d3ddf580e196795
scott <scott@beanstalk.com>
2015-03-05 21:26:01 -0500

    bump to beanstalk 5.0050
    Change-Id: I20807c8ad09edb3453bef2194773964326f3ff66
    Signed-off-by: scott <scott@beanstalk.com>

--- config/common.mk
+++ config/common.mk
@@ -213,7 +213,7 @@ PRODUCT_COPY_FILES += \
 	vendor/beanstalk/prebuilt/etc/hosts.alt:system/etc/hosts.alt \
 	vendor/beanstalk/prebuilt/etc/hosts.og:system/etc/hosts.og
 
-BeanStalk_Version=5.0040
+BeanStalk_Version=5.0050
 BS_VERSION := BeanStalk-$(BeanStalk_Version)-$(shell date -u +%Y%m%d)-$(BS_BUILD)
 
 PRODUCT_PROPERTY_OVERRIDES += \
