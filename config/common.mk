df0cd2bfb3b3d7abb429f1efb0fd0da80dee3e46
wrapped <ddroidx@gmail.com>
2015-11-30 14:52:34 -0600

    update some prebuilts.
    Change-Id: I894e902fe414161bdf0c39733bf0a1cfa1a4cbfc
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	configs/common.mk

--- config/common.mk
+++ config/common.mk
@@ -110,6 +110,13 @@ PRODUCT_PACKAGES += \
     BluetoothExt \
     Profiles
 
+# DU Utils Library
+PRODUCT_PACKAGES += \
+    org.dirtyunicorns.utils
+
+PRODUCT_BOOT_JARS += \
+    org.dirtyunicorns.utils
+
 # Optional CM packages
 PRODUCT_PACKAGES += \
     libemoji \
