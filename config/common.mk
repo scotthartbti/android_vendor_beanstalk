a5571a41534e1a9fc94abbd070c9e36a723d27cb
scott <scott@ubuntu>
2015-12-03 23:33:18 -0500

    Use prebuilt latinIME, remove call to build latinIME and delete dict overlay
    Change-Id: Icc22781055062fadffe6e75840a3412f30c9c845
    Signed-off-by: scott <scott@ubuntu>

--- config/common.mk
+++ config/common.mk
@@ -85,11 +85,13 @@ PRODUCT_COPY_FILES += \
     vendor/beanstalk/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/beanstalk/prebuilt/common/bin/sysinit:system/bin/sysinit
 
-ifneq ($(TARGET_BUILD_VARIANT),user)
 # userinit support
 PRODUCT_COPY_FILES += \
     vendor/beanstalk/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
-endif
+
+# latinIME lib needed for swipe
+PRODUCT_COPY_FILES += \
+    vendor/beanstalk/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
 
 # CM-specific init file
 PRODUCT_COPY_FILES += \
