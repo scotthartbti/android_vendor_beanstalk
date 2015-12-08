0dffd5117013a3ad9ba3772ddd0ccee68ad4b44a
scott <scott@ubuntu>
2015-12-08 06:44:00 -0500

    bump to BeanStalk 6.01
    Change-Id: I56b24e485ecfae780922ca70bbfef31509d5a757
    Signed-off-by: scott <scott@ubuntu>

--- config/common.mk
+++ config/common.mk
@@ -206,7 +206,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/beanstalk/overlay/common
 
-BeanStalk_Version=6.00
+BeanStalk_Version=6.01
 BS_VERSION := BeanStalk-$(BeanStalk_Version)-$(shell date -u +%Y%m%d)-$(BS_BUILD)
 
 PRODUCT_PROPERTY_OVERRIDES += \
