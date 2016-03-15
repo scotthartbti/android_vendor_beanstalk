509ed18fa45902fe3dda212f5c5439701b2ca955
Adnan Begovic <adnan@cyngn.com>
2016-03-15 14:17:47 -0700

    cm: Commonize cmsdk specific targets.
    Change-Id: I5e2f0b84ed34244b7549daf91053258193dd716f
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	config/common.mk
    	config/themes_common.mk

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -3,6 +3,3 @@ PRODUCT_PACKAGES += \
     aapt \
     ThemeChooser \
     ThemesProvider
-
-PRODUCT_COPY_FILES += \
-    vendor/beanstalk/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
