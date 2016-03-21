e6db1096070eac0c68d68ec30cde41c1b02d2bca
Luis Vidal <lvidal@cyngn.com>
2016-03-21 10:18:29 -0700

    Add Weather Content Provider [3/5]
    Introduce the weather system feature, which will be used to
    identify if the Weather Content Provider/Weather services are
    available in the device.
    
    Add SELinux entries for the cmweather service
    
    Change-Id: Ibe862903095276f87f23c0d7dae54733eeeb5638
    Signed-off-by: scott <scott.hart.bti@gmail.com>
    
    Conflicts:
    	config/cmsdk_common.mk

--- config/cmsdk_common.mk
+++ config/cmsdk_common.mk
@@ -7,7 +7,8 @@ PRODUCT_COPY_FILES += \
     vendor/beanstalk/config/permissions/org.cyanogenmod.appsuggest.xml:system/etc/permissions/org.cyanogenmod.appsuggest.xml \
     vendor/beanstalk/config/permissions/org.cyanogenmod.telephony.xml:system/etc/permissions/org.cyanogenmod.telephony.xml \
     vendor/beanstalk/config/permissions/org.cyanogenmod.performance.xml:system/etc/permissions/org.cyanogenmod.performance.xml \
-    vendor/beanstalk/config/permissions/org.cyanogenmod.partner.xml:system/etc/permissions/org.cyanogenmod.partner.xml
+    vendor/beanstalk/config/permissions/org.cyanogenmod.partner.xml:system/etc/permissions/org.cyanogenmod.partner.xml \
+    vendor/beanstalk/config/permissions/org.cyanogenmod.weather.xml:system/etc/permissions/org.cyanogenmod.weather.xml
 
 # CM Platform Library
 PRODUCT_PACKAGES += \
