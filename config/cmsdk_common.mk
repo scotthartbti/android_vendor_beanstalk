ed60d8fd40b6570fc0b15d94d481b105c00dbb03
Steve Kondik <steve@cyngn.com>
2016-04-23 00:54:50 -0700

    cm: Add CMAudioService the platform
     * Also brings JNI to CMSDK
    
    Change-Id: I599964a1f9200a8d2ecdad0bb8c4d8593e6d7415
    Signed-off-by: scott <scott.hart.bti@gmail.com>
    
    Conflicts:
    	config/cmsdk_common.mk

--- config/cmsdk_common.mk
+++ config/cmsdk_common.mk
@@ -1,6 +1,7 @@
 # Permissions for cmsdk services
 PRODUCT_COPY_FILES += \
     vendor/beanstalk/config/permissions/org.cyanogenmod.appsuggest.xml:system/etc/permissions/org.cyanogenmod.appsuggest.xml \
+    vendor/beanstalk/config/permissions/org.cyanogenmod.audio.xml:system/etc/permissions/org.cyanogenmod.audio.xml \
     vendor/beanstalk/config/permissions/org.cyanogenmod.livedisplay.xml:system/etc/permissions/org.cyanogenmod.livedisplay.xml \
     vendor/beanstalk/config/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml \
     vendor/beanstalk/config/permissions/org.cyanogenmod.partner.xml:system/etc/permissions/org.cyanogenmod.partner.xml \
@@ -20,6 +21,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     org.cyanogenmod.hardware \
     org.cyanogenmod.hardware.xml
+
+# JNI Libraries
+PRODUCT_PACKAGES += \
+    libcmsdk_platform_jni
 
 ifndef CM_PLATFORM_SDK_VERSION
   # This is the canonical definition of the SDK version, which defines
