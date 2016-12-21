6a54184b75d7d62c6a8b7c6a01344719db337ba1
scotthartbti <scott.hart.bti@gmail.com>
2016-12-20 22:16:07 -0500

    fix path from previous commit
    Change-Id: I44eece43484884389b612e7e2de5d25baf53fcc0
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- config/telephony.mk
+++ config/telephony.mk
@@ -14,4 +14,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # IMS Default Permission
 PRODUCT_COPY_FILES += \
-    vendor/cm/config/permissions/qcom_ims.xml:system/etc/default-permissions/qcom_ims.xml
+    vendor/beanstalk/config/permissions/qcom_ims.xml:system/etc/default-permissions/qcom_ims.xml
