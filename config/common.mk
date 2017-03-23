2c5ab333a2f0b3d39580af70104139b4b67e3017
Danny Baumann <dannybaumann@web.de>
2017-03-23 10:44:43 +0100

    Add email app to device idle whitelist.
    It needs permanent network access for IMAP IDLE.
    
    Change-Id: I460f389253fd5d4bbcfeb4ea6b7449f19a6b7abd
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -63,9 +63,10 @@ PRODUCT_COPY_FILES += \
     vendor/beanstalk/prebuilt/common/bin/50-cm.sh:system/addon.d/50-cm.sh \
     vendor/beanstalk/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 
-# Backup Services whitelist
+# System feature whitelists
 PRODUCT_COPY_FILES += \
-    vendor/beanstalk/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
+    vendor/beanstalk/config/permissions/backup.xml:system/etc/sysconfig/backup.xml \
+    vendor/beanstalk/config/permissions/power-whitelist.xml:system/etc/sysconfig/power-whitelist.xml
 
 # Signature compatibility validation
 PRODUCT_COPY_FILES += \
